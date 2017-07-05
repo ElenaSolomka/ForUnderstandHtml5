9594f7804e36297afa7c652b6ff36a969bc6eae9
Solodovnikova Elena <solodovnikovaem@kurs.corp>
2017-07-05 13:30:00 +0300

    Modern Inherit

---
+++ js_InheritModern_ExtendDeep.js
@@ -0,0 +1,21 @@
+﻿function extendDeep(parent, child) {
+    var i,
+        toString = Object.prototype.toString,
+        aStr = "[object Array]";
+
+    child = child || {}; // проверка наличия второго аргумента.
+
+    for (i in parent) {
+        if (parent.hasOwnProperty(i)) {
+            if (typeof parent[i] == "object") {
+                child[i] = (toString.call(parent[i]) == aStr) ? [] : {};
+                extendDeep(parent[i], child[i]); // рекурсивный вызов, для того что бы скопировать все свойства объякта или элементы массива.
+            }
+            else {
+                child[i] = parent[i];
+            }
+        }
+    }
+
+    return child;
+}
---
+++ js_InheritModern_Mix.js
@@ -0,0 +1,25 @@
+﻿function mix() {
+    var arg, prop, child = {};
+    for (arg = 0; arg < arguments.length; arg++) {
+        for (prop in arguments[arg]) {
+            if (arguments[arg].hasOwnProperty(prop)) {
+                child[prop] = arguments[arg][prop];
+            }
+        }
+    }
+
+    return child;
+}
+function mixExtend() {
+    var arg, prop, child = {};
+    for (arg = 0; arg < arguments.length; arg++) {
+        extendDeep(arguments[arg], child)
+        //for (prop in arguments[arg]) {
+        //    if (arguments[arg].hasOwnProperty(prop)) {
+        //        child[prop] = arguments[arg][prop];
+        //    }
+        //}
+    }
+
+    return child;
+}
